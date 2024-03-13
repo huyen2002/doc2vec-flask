4a8fe403f2a3394897fc0714059c5e1ab89cfcef
Huyền Nguyễn <thanhhuyen11102k2@gmail.com>
2024-03-13 22:43:36 +0700

    feat: change data train

--- app.py
+++ app.py
@@ -3,29 +3,34 @@ from flask import Flask, request, jsonify
 from pyvi import ViTokenizer
 import gensim
 
-
+# model.translate_file()
 model.train_model()
 doc2vec_model = model.load_model()
 
 app = Flask(__name__)
 
+
 @app.route('/api/top_n_most_similar', methods=['POST'])
 def api_get_top_n_similar():
-	data = request.get_json()
-	vec = doc2vec_model.infer_vector(model.tokenize_word(data['data']))
-	res = doc2vec_model.docvecs.most_similar([vec], topn = 5)
-	
-	return jsonify(list=res)
+    if request.is_json:
+        data = request.get_json()
+        print(data)
+        vec = doc2vec_model.infer_vector(model.tokenize_word(data['data']))
+        print(vec)
+        res = doc2vec_model.docvecs.most_similar([vec], topn=5)
+        return jsonify(list=res)
+    else:
+        return jsonify(error="Request is not json"), 400
 
 
 @app.route('/api/compare_two_documents', methods=['POST'])
 def api_compare_two_documents():
-	data = request.get_json()
-	vec_1 = doc2vec_model.infer_vector(model.tokenize_word(data['doc_1']))
-	vec_2 = doc2vec_model.infer_vector(model.tokenize_word(data['doc_2']))
+    data = request.get_json()
+    vec_1 = doc2vec_model.infer_vector(model.tokenize_word(data['doc_1']))
+    vec_2 = doc2vec_model.infer_vector(model.tokenize_word(data['doc_2']))
 
-	vec_1 = gensim.matutils.full2sparse(vec_1)
-	vec_2 =gensim.matutils.full2sparse(vec_2)
+    vec_1 = gensim.matutils.full2sparse(vec_1)
+    vec_2 = gensim.matutils.full2sparse(vec_2)
 
-	similarity = gensim.matutils.cossim(vec_1, vec_2)
-	return jsonify(2)
+    similarity = gensim.matutils.cossim(vec_1, vec_2)
+    return jsonify(2)
--- model.py
+++ model.py
@@ -4,41 +4,50 @@ import gensim
 from pyvi import ViTokenizer
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import string
+from deep_translator import GoogleTranslator
 
-data =[]
+data = []
 
 stops = open("stopwords_vietnamese.txt", "r", encoding="utf8").read().split()
+
+
+def translate_file():
+    translated = GoogleTranslator(source='auto', target='vi').translate_file('introduce_data/UK_BSC_I100.txt')
+    print(translated)
+
 
 def tokenize_word(doc):
     doc_remove_digit = (''.join([c for c in doc if not c.isdigit()])).lower()
     token_words = ViTokenizer.tokenize(doc_remove_digit).split()
-    token_words_no_punct =[t for t in token_words if t not in stops and t not in string.punctuation]
+    token_words_no_punct = [t for t in token_words if t not in stops and t not in string.punctuation]
     return token_words_no_punct
 
 
 def train_model():
-    
     train_corpus_dir = "introduce_data"
-    docLabels = [file for file in listdir(train_corpus_dir) if file.endswith(".txt")]
-    # print(docLabels)
+    doc_labels = [file for file in listdir(train_corpus_dir) if file.endswith(".txt")]
+    # print(doc_labels)
 
-    for doc in docLabels:
-        file = open(join(train_corpus_dir,doc), "r", encoding="utf8")
-        data.append(file.read())
-
+    for doc in doc_labels:
+        if doc.startswith('VN'):
+            file = open(join(train_corpus_dir, doc), "r", encoding="utf8")
+            data.append(file.read())
+        else:
+            translated = GoogleTranslator(source='auto', target='vi').translate_file(join(train_corpus_dir, doc))
+            data.append(translated)
 
     for i in range(len(data)):
         data[i] = tokenize_word(data[i])
 
-   # tag data with tags is docLabels
-    tagged_data = [TaggedDocument(words=data[i], tags=[docLabels[i]]) for i in range(len(data))]
-    print(tagged_data)
+        # tag data with tags is doc_labels
+    tagged_data = [TaggedDocument(words=data[i], tags=[doc_labels[i]]) for i in range(len(data))]
+    # print(tagged_data)
     model = gensim.models.doc2vec.Doc2Vec(vector_size=100, dm=1, window=5, min_count=2, epochs=100)
 
     model.build_vocab(tagged_data)
     model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
 
-    similar_doc = model.docvecs.most_similar(docLabels[0])
+    similar_doc = model.docvecs.most_similar(doc_labels[0])
     print(similar_doc)
     model.save("doc2vec.model")
 
