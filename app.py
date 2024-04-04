9bf2fc8fe32dcb3991db946807b8c2637f1ef2f0
Huyền Nguyễn <thanhhuyen11102k2@gmail.com>
2024-04-04 22:55:21 +0700

    feat: load pre-trained model and fix api with test data

--- app.py
+++ app.py
@@ -1,39 +1,67 @@
+from deep_translator import GoogleTranslator
+from pyvi.ViTokenizer import ViTokenizer
+
 import model
 from flask import Flask, request, jsonify
 import gensim
 
 doc2vec_model = model.load_model()
-
+print("hello")
 app = Flask(__name__)
 
+print(ViTokenizer.tokenize("Bạn dạo này có khỏe không?").split())
 
-@app.route('/api/top_n_most_similar', methods=['POST'])
-def api_get_top_n_similar():
-    if request.is_json:
-        data = request.get_json()
-        print(data)
-        doc2vec_model.random.seed(0)
-        vec = doc2vec_model.infer_vector(model.tokenize_word(data['data']))
-        print(vec)
-        res = doc2vec_model.docvecs.most_similar([vec], topn=8)
-        return jsonify(list=res)
-    else:
-        return jsonify(error="Request is not json"), 400
+doc_labels, doc_list = model.load_data()
+other_document_vectors = model.process_data()
+
+
+# @app.route('/api/top_n_most_similar', methods=['POST'])
+# def api_get_top_n_similar():
+#     if request.is_json:
+#         data = request.get_json()
+#         # doc2vec_model.random.seed(0)
+#         vec = doc2vec_model.infer_vector(model.tokenize_word(data['data']))
+#         print(vec)
+#         res = doc2vec_model.docvecs.most_similar([vec], topn=8)
+#         return jsonify(list=res)
+#     else:
+#         return jsonify(error="Request is not json"), 400
 
 
 @app.route('/api/compare_two_documents', methods=['POST'])
 def api_compare_two_documents():
     data = request.get_json()
     doc2vec_model.random.seed(0)
+
     vec_1 = doc2vec_model.infer_vector(model.tokenize_word(data['document_1']))
     doc2vec_model.random.seed(0)
     vec_2 = doc2vec_model.infer_vector(model.tokenize_word(data['document_2']))
 
-    print('vec1', vec_1)
-    print('vec2', vec_2)
     vec_1 = gensim.matutils.full2sparse(vec_1)
     vec_2 = gensim.matutils.full2sparse(vec_2)
 
     similarity = gensim.matutils.cossim(vec_1, vec_2)
     print(similarity)
     return jsonify(similarity=similarity)
+
+
+@app.route('/api/top_n_most_similar', methods=['POST'])
+def api_get_top_8_similar():
+    similarities = []
+    if request.is_json:
+        data = request.get_json()
+        doc2vec_model.random.seed(0)
+        target_document_vector = doc2vec_model.infer_vector(model.tokenize_word(data['data']))
+        vec_target = gensim.matutils.full2sparse(target_document_vector)
+        for other_vector in other_document_vectors:
+            vec_other = gensim.matutils.full2sparse(other_vector)
+            similarity = gensim.matutils.cossim(vec_target, vec_other)
+            similarities.append(similarity)
+        # Find the indices of the top 5 most similar documents
+        top_5_indices = sorted(range(len(similarities)), key=lambda i: similarities[i], reverse=True)[:8]
+        # Get the top 5 most similar documents label and similarity
+        top_5_similar_documents = [(doc_labels[i], similarities[i]) for i in top_5_indices]
+
+        return jsonify(list=top_5_similar_documents)
+    else:
+        return jsonify(error="Request is not json"), 400
--- model.py
+++ model.py
@@ -5,19 +5,30 @@ from pyvi import ViTokenizer
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import string
 from deep_translator import GoogleTranslator
+from langdetect import detect
 
 data = []
 
-stops = open("stopwords_vietnamese.txt", "r", encoding="utf8").read().split()
-
-
-def translate_file():
-    translated = GoogleTranslator(source='auto', target='vi').translate_file('introduce_data/UK_BSC_I100.txt')
-    print(translated)
+stops = open("stopwords_vietnamese.txt", "r", encoding="utf8").readline().split()
 
 
 def tokenize_word(doc):
-    doc_remove_digit = (''.join([c for c in doc if not c.isdigit()])).lower()
+    target_doc = doc
+    lang = detect(doc)
+    if lang != 'vi':
+        if len(doc) > 4999:
+            translated = ""
+            for i in range(0, len(doc), 4999):
+                translated += GoogleTranslator(source='auto', target='vi').translate(doc[i:i + 4999])
+
+        else:
+            translated = GoogleTranslator(source='auto', target='vi').translate(doc)
+
+            target_doc = translated
+    else:
+        target_doc = doc
+
+    doc_remove_digit = (''.join([c for c in target_doc if not c.isdigit()])).lower()
     token_words = ViTokenizer.tokenize(doc_remove_digit).split()
     token_words_no_punct = [t for t in token_words if t not in stops and t not in string.punctuation]
     return token_words_no_punct
@@ -45,16 +56,41 @@ def train_model():
     model = gensim.models.doc2vec.Doc2Vec(vector_size=50, dm=1, window=5, min_count=2, epochs=100, workers=5)
 
     model.build_vocab(tagged_data)
-    model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs )
+    model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
 
-    similar_doc = model.docvecs.most_similar(doc_labels[0])
-    print(similar_doc)
+    similar_doc = model.docvecs.most_similar(doc_labels[0], )
     model.save("doc2vec.model")
 
 
 def load_model():
     try:
         return Doc2Vec.load("doc2vec.model")
-    except:
-        print("model is not found")
+    except Exception as e:
+        print(e)
         return None
+
+
+def load_data():
+    train_corpus_dir = "introduce_data"
+    doc_labels = [file for file in listdir(train_corpus_dir) if file.endswith(".txt")]
+    # print(doc_labels)
+    doc_list = []
+    for doc in doc_labels:
+        file = open(join(train_corpus_dir, doc), "r", encoding="utf8")
+        doc_list.append(file.read())
+
+    return doc_labels, doc_list
+
+
+def process_data():
+    model = load_model()
+    doc_labels, doc_list = load_data()
+
+    document_vectors = []
+    for i in range(len(doc_list)):
+        doc = doc_list[i]
+        tokenized_doc = tokenize_word(doc)
+        document_vector = model.infer_vector(tokenized_doc)
+        document_vectors.append(document_vector)
+
+    return document_vectors
